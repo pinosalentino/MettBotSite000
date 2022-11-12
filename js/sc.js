b6501495c5e342ea84486efefe39da5a68b69267
LoryPelli <87276663+LoryPelli@users.noreply.github.com>
2022-11-12 18:53:35 +0100

    Fixed Everything

--- js/sc.js
+++ js/sc.js
@@ -1,12 +1,12 @@
-function görünebilir(){
+function görünebilir() {
     var ID;
     ID = document.getElementById("detaylar");
 
     if (ID.style.visibility == "hidden") {
-            ID.style.visibility = "visible";
-            console.log("Kullanıcı destek bilgerini görüntüledi.");
-        } else {
-            ID.style.visibility = "hidden";
-            console.log("Kullanıcı destek bilgerini kapattı.");
+        ID.style.visibility = "visible";
+        console.log("Kullanıcı destek bilgerini görüntüledi.");
+    } else {
+        ID.style.visibility = "hidden";
+        console.log("Kullanıcı destek bilgerini kapattı.");
     }
 }
